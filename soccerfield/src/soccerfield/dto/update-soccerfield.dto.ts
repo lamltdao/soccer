aafd6edb0e651094a30ef9b3379ff8d8fcd4190f
lamdao <daoletunglam2002@gmail.com>
2021-07-15 09:52:32 +0700

    recreate soccerfield srv

--- soccerfield/src/soccerfield/dto/update-soccerfield.dto.ts
+++
@@ -1,6 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateSoccerfieldDto } from './create-soccerfield.dto';
-
-export class UpdateSoccerfieldDto extends PartialType(CreateSoccerfieldDto) {
-  id: number;
-}
